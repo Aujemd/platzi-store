c6265a08cd502e7d6953c53eab3d3cdb64a661d1
Johan Mora <joenmodu99@gmail.com>
2020-10-28 20:48:20 -0400

    SnapShots

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -10,5 +11,12 @@ describe('<Footer />', () => {
   });
   test('Render del titulo', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => { //Testeando Ui que no cambia
+  test('Footer UI test', () => {
+    const footer = create(<Footer />).toJSON();
+    expect(footer).toMatchSnapshot();
   });
 });
